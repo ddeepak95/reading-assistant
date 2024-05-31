ccbacad820ab5196f459f119d4abbd801a241e56
ddeepak95 <ddeepak95@gmail.com>
2024-05-30 17:25:09 -0700

    get the content from adjacent elements

--- epub_functions.py
+++ epub_functions.py
@@ -28,29 +28,33 @@ def get_chapters_from_epub(epub_file_obj):
                 chapter['title'] = link.title
                 chapter['href'] = link.href
                 chapters.append(chapter)
+ 
+    for chapter in chapters:
+        href_base, _, href_locator = chapter['href'].partition('#')
+        if chapters.index(chapter) < len(chapters) - 1:
+            next_chapter = chapters[chapters.index(chapter) + 1]
+            next__chapter_href_base, _, next_chapter_href_locator = next_chapter['href'].partition('#')
 
-    # for chapter in chapters:
-    #     href_base, _, href_locator = chapter['href'].partition('#')
-    #     item = book.get_item_with_href(href_base)
-    #     if item:
-    #         soup = BeautifulSoup(item.get_body_content(), 'html.parser')
-    #         if href_locator:
-    #             element = soup.find(id=href_locator)
-    #             chapter['content'] = element.get_text() if element else ''
-    #         else:
-    #             chapter['content'] = soup.get_text()
-    st.write(chapters)
-    href="OEBPS/part0008.xhtml"
-    href_base, _, href_locator = href.partition('#')
-    item = book.get_item_with_href(href_base)
-    st.write(item.get_body_content())
-    if item:
-        soup = BeautifulSoup(item.get_body_content(), 'html.parser')
-        if href_locator:
-            element = soup.find(id=href_locator)
-            chapter['content'] = element.get_text() if element else ''
-        else:
-            chapter['content'] = soup.get_text()
+        item = book.get_item_with_href(href_base)
+        if item:
+            soup = BeautifulSoup(item.get_body_content(), 'html.parser')
+            if href_locator:
+                element = soup.find(id=href_locator)
+                content = []
+                if element:
+                    next_elements = [element] + list(element.find_all_next())
+                    
+                    for next_element in next_elements:
+                        if next_chapter_href_locator != "" and next_element.get('id') == next_chapter_href_locator:
+                            break
+                        if next_element.string:
+                            content.append(next_element.string)
+                    chapter['content'] = ' '.join(content)
+                else:
+                    chapter['content'] = ''
+            else:
+                chapter['content'] = soup.get_text()
+    
 
 
 
