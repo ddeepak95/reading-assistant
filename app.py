11b8ab6d14abd62dc7ee4f1ddf7ea0e7716efa40
ddeepak95 <ddeepak95@gmail.com>
2024-06-24 12:09:05 +0530

    get toc details and doc files in the epub

--- app.py
+++ app.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 from handler import Application
 
 notion_parent_page_id = os.getenv('NOTION_PARENT_PAGE_ID')
-book_path = './books/buddhism/book.epub'
-html_path = './books/buddhism/highlights.html'
-book_title = "Why Buddhism is True"
+book_path = './books/accelerating-ind/book.epub'
+html_path = './books/four-thousand-weeks/highlights.html'
+book_title = "Four Thousand Weeks: Time Management for Mortals"
 chapters_to_skip = ["cover", "epigraph", "acknowledgements", "acknowledgment", "foreword", "dedication", "contents", "introduction", "prologue", "appendix", "bibliography", "index", "glossary", "about the author", "about the book", "about the publisher", "colophon", "title page", "blank page", "half title", "blank", "notes", "copyright"]
-summary_instructions = "Focus on the buddhism concepts and the scientific ideas shared."
+summary_instructions = "Focus on the ideas by different thinkers and phiolosophies. Also, focus on the techniques as well as strategies shared for time management."
 
 if __name__ == '__main__':
     app = Application(book_path, html_path, summary_instructions, notion_parent_page_id, book_title, chapters_to_skip)
--- epub_functions.py
+++ epub_functions.py
@@ -8,7 +8,37 @@ class Book:
         self.book_source = book_source
         self.chapters_to_skip = chapters_to_skip
         self.book_content = epub.read_epub(self.book_source)
+        self.toc_details = self._get_toc_details()
+        self.files_in_epub = self._get_files_in_epub()
         self.chapters = self._get_chapters()
+    
+    def _get_toc_details(self):
+        toc = self.book_content.toc
+        toc_details = []
+        for item in toc:
+            if isinstance(item, ebooklib.epub.Link):
+                toc_item ={"type": "link", "href": item.href, "title": item.title, "uid": item.uid}
+
+            elif isinstance(item, tuple) and isinstance(item[0], ebooklib.epub.Section):
+                toc_item = {"type": "section", "title": item[0].title, "links": []}
+                for link in item[1]:
+                    toc_item["links"].append({"type": "link", "href": link.href, "title": link.title, "uid": link.uid})
+            toc_details.append(toc_item)
+        return toc_details
+
+    def _get_files_in_epub(self):
+        items = self.book_content.get_items()
+        files = []
+        for file_item in items:
+            item_id = file_item.get_id()
+            item_type = file_item.get_type()
+            item_name = file_item.get_name()
+            if item_type == 9:
+                # get title of file by parsing html
+                soup = BeautifulSoup(file_item.get_content(), 'html.parser')
+                title = soup.title.string if soup.title else 'No Title'
+                files.append({"file_id": item_id, "file_name": item_name, "title": title})
+        return files
 
     def _get_chapters(self):
         print("Getting chapters from the book")
